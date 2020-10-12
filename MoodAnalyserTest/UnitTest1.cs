fe27431c0a66fd6c70c0988e29e6c9f35094de4a
Administrator <siddhiseth3009@gmail.com>
2020-10-12 15:33:04 +0530

    [Siddhi]Add. TC 3.1 & TC 3.2 added

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -25,5 +25,33 @@ namespace MoodAnalyserTest
             //Assert
             Assert.AreEqual("Happy", mood);
         }
+        [TestMethod]
+        public void Given_Empty_Mood_Should_Throw_MoodAnalysisException_Indicating_EmptyMood()
+        {
+            try
+            {
+                string message = string.Empty;
+                MoodAnalyser mood = new MoodAnalyser(message);
+                string moodStr = mood.AnalyseMood();
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                Assert.AreEqual("Mood should not be empty", e.Message);
+            }
+        }
+        [TestMethod]
+        public void Given_NULL_Mood_Should_Throw_MoodAnalysisException()
+        {
+            try
+            {
+                string message = null;
+                MoodAnalyser moodAnalyse = new MoodAnalyser(message);
+                string mood = moodAnalyse.AnalyseMood();
+            }
+            catch (MoodAnalyserCustomException e)
+            {
+                Assert.AreEqual("Mood should not be null", e.Message);
+            }
+        }
     }
 }
