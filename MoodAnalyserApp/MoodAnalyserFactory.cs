b58ebb6a973560c84fd8d8c2fdab88161a39683f
Administrator <siddhiseth3009@gmail.com>
2020-10-14 15:49:41 +0530

    [Siddhi]Add. MoodAnalyserObject creation with parameterised constructor method added

--- MoodAnalyserApp/MoodAnalyserFactory.cs
+++ MoodAnalyserApp/MoodAnalyserFactory.cs
@@ -31,5 +31,22 @@ namespace MoodAnalyserApp
                 throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
             }
         }
+        public static object CreateObjectOfMoodAnalyserUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo info = type.GetConstructor(new[] { typeof(string) });
+                    object instance = info.Invoke(new object[] { (message) });
+                    return instance;
+                }
+                else
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor not found");
+            }
+            else
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "No such class found");
+        }
     }
 }
