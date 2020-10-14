e9d047c8e37557f9180f85876044ed568c1fc184
Administrator <siddhiseth3009@gmail.com>
2020-10-14 17:03:43 +0530

    [Siddhi]Refactor InvokeAnalyseMood added in MoodAnalyserReflector

---
+++ MoodAnalyserApp/MoodAnalyserReflector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace MoodAnalyserApp
+{
+    class MoodAnalyserReflector
+    {
+        public static string InvokeAnalyseMood(string message, string methodName)
+        {
+            try
+            {
+                Type type = Type.GetType("MoodAnalyserApp.MoodAnalyser");
+                object moodAnalyserObject = MoodAnalyserFactory.CreateObjectOfMoodAnalyserUsingParameterizedConstructor("MoodAnalyserApp.MoodAnalyser", "MoodAnalyser", message);
+                MethodInfo method = type.GetMethod(methodName);
+                object mood = method.Invoke(moodAnalyserObject, null);
+                return mood.ToString();
+            }
+            catch (NullReferenceException e)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "No such method found");
+            }
+        }
+    }
+}
