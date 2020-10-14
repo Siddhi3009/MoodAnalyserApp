af340ab0ae8c03b0ca653a533489bdc8d2580676
Administrator <siddhiseth3009@gmail.com>
2020-10-14 14:37:09 +0530

    [Siddhi]Refactor. Test case names refactored

--- MoodAnalyserTest/UnitTest1.cs
+++ MoodAnalyserTest/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace MoodAnalyserTest
     public class UnitTest1
     {
         [TestMethod]
-        public void MoodAnalyser_Should_Return_Sad()
+        public void GivenSadMessage_ShouldReturn_SadMood()
         {
             //Arrange
             MoodAnalyser analyser = new MoodAnalyser("I am in a Sad mood");
@@ -16,7 +16,7 @@ namespace MoodAnalyserTest
             Assert.AreEqual("Sad", mood);
         }
         [TestMethod]
-        public void MoodAnalyser_Should_Return_Happy()
+        public void GivenHappyMessage_ShouldReturn_HappyMood()
         {
             //Arrange
             MoodAnalyser analyser = new MoodAnalyser("I am in a Happy mood");
@@ -26,7 +26,7 @@ namespace MoodAnalyserTest
             Assert.AreEqual("Happy", mood);
         }
         [TestMethod]
-        public void MoodAnalyser_Should_Return_Happy_For_Invalid_Input()
+        public void GivenInvalidInput_ShouldReturn_HappyMood()
         {
             //Arrange
             MoodAnalyser analyser = new MoodAnalyser();
